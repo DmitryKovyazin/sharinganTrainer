c7cf5d40d9d5685878b72c9ea1f34904e351a54b
DmitryK <demonitachi2391@gmail.com>
2025-12-17 16:04:07 +0300

    added className function and ceate gitignore

--- src/App.tsx
+++ src/App.tsx
@@ -5,14 +5,17 @@ import { Suspense} from 'react';
 import { AboutPageAsync } from './pages/AboutPage/AboutPage.async';
 import { MainPageAsync } from './pages/MainPage/MainPage.async';
 import { UseTheme } from './theme/UseTheme';
+import { classNames } from './helpers/classNames/classNames';
 
 //перечисление для хранения тем)
 
 function App() {
+
+  
   const { theme, toggleTheme } = UseTheme();
 
   return (
-    <div className={`app ${theme}`}>
+    <div className={classNames('app', {}, [theme])}>
       <button onClick={toggleTheme}>toggle Theme</button>
       <Link to={'/'}>Main</Link>
       <Link to={'/about'}>About</Link>
